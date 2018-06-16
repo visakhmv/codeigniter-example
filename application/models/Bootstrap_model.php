4145a5917009f4cec404c374c4df8ea682c3ebaa
Visakh MV <visakhmv143@gmail.com>
2018-06-16 12:10:02 +0530

    Source format

--- application/models/Bootstrap_model.php
+++ application/models/Bootstrap_model.php
@@ -1,27 +1,28 @@
 <?php
 class Bootstrap_model extends CI_Model
 {
-    function getAllCountries($search,$count=false,$from=0,$limit=null)
+    public function getAllCountries($search, $count = false, $from = 0, $limit = null)
     {
-        if($search){
-            $this->db->like('name',$search);
+        if ($search) {
+            $this->db->like('name', $search);
         }
         $this->db->select('code,name,continent,region,population');
         /* Same as
         $query = $this->db->query('SELECT * FROM test');
          */
         // Returns the results as an array
-        if($count){
+        if ($count) {
             return $this->db->get('country')->num_rows();
-        }else{
-            $this->db->limit($limit,$from);
+        } else {
+            $this->db->limit($limit, $from);
             return $this->db->get('country')->result_array();
         }
     }
 
-    function searchCounty($term){
+    public function searchCounty($term)
+    {
         $this->db->select('code as id,name as value');
-        $this->db->like('name',$term);
+        $this->db->like('name', $term);
         $this->db->limit('10');
         return $this->db->get('country')->result_array();
     }
