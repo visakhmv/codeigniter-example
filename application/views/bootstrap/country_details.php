7cdf60fb8986471fa5ddaa375ea2541b89a299d7
Visakh MV <visakhmv143@gmail.com>
2018-06-18 11:56:36 +0530

    Intent changes

--- application/views/bootstrap/country_details.php
+++ application/views/bootstrap/country_details.php
@@ -1,9 +1,9 @@
 <div class="col-sm-10">
     <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="<?= base_url('bootstrap/country') ?>">Countries</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Country Details</li>
-    </ol>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?= base_url('bootstrap/country') ?>">Countries</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Country Details</li>
+        </ol>
     </nav>
     <p class="h3">Country</p>
     <div class="row card-body">
